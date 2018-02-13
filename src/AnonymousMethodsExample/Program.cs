bf3c5c613810012dc9012fb60aee0e200f012e1b
John Darvill <john.darvill@experian.com>
2018-02-13 16:46:40 +0000

    Adding files.

---
+++ src/AnonymousMethodsExample/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesExample
+{
+    class BigRedButton
+    {
+        private Action onButtonPush;
+
+        public BigRedButton(Action onButtonPush)
+        {
+            this.onButtonPush = onButtonPush;
+        }
+
+        public void Push()
+        {
+            this.onButtonPush();
+        }
+    }
+
+    class BigMassiveRocket
+    {
+        public void Launch()
+        {
+            Console.WriteLine("Launching Big Massive Rocket!!!");
+        }
+    }
+
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            BigMassiveRocket rocket = new BigMassiveRocket();
+            BigRedButton button = new BigRedButton(rocket.Launch);
+
+            string userInput = string.Empty;
+
+            while (userInput != "q")
+            {
+                if (userInput == "push")
+                {
+                    Action pushMethod = button.Push;
+
+                    pushMethod();
+                }
+
+                userInput = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ src/DelegatesExample/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesExample
+{
+    delegate void ButtonPushHandler();
+
+    class BigRedButton
+    {
+        private ButtonPushHandler onButtonPush;
+
+        public void RegisterHandler(ButtonPushHandler handler)
+        {
+            this.onButtonPush = handler;
+        }
+
+        public void Push()
+        {
+            this.onButtonPush();
+        }
+    }
+
+    class BigMassiveRocket
+    {
+        public void Launch()
+        {
+            Console.WriteLine($"Launching Big Massive Rocket!!!");
+        }
+    }
+
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            BigMassiveRocket rocket = new BigMassiveRocket();
+            BigRedButton button = new BigRedButton();
+
+            button.RegisterHandler(rocket.Launch);
+
+            string userInput = string.Empty;
+
+            while (userInput != "q")
+            {
+                if (userInput == "push")
+                {
+                    button.Push();
+                }
+
+                userInput = Console.ReadLine();
+            }
+        }
+    }
+}
